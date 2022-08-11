d04c47fd7c92748319f63af844e008d408e37117
Johannes Leuschner <jleuschn@uni-bremen.de>
2022-08-11 15:59:24 +0200

    evaluation scripts

--- evaluation/evaluate_kmnist_sample_based_density.py
+++ evaluation/evaluate_kmnist_sample_based_density.py
@@ -5,7 +5,7 @@ import torch
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--runs_file', type=str, default='runs_kmnist_exact_density.yaml', help='path of yaml file containing hydra output directory names')
+parser.add_argument('--runs_file', type=str, default='runs_kmnist_sample_based_density.yaml', help='path of yaml file containing hydra output directory names')
 parser.add_argument('--experiments_outputs_path', type=str, default='../experiments/outputs', help='base path containing the hydra output directories (usually "[...]/outputs/")')
 parser.add_argument('--print_individual_log_probs', action='store_true', default=False)
 args = parser.parse_args()
@@ -14,9 +14,9 @@ with open(args.runs_file, 'r') as f:
     runs = yaml.safe_load(f)
 
 NOISE_LIST = [0.05, 0.1]
-ANGLES_LIST = [5, 10, 20, 30]
-INCLUDE_PREDCP_LIST = [True, False]
-NUM_IMAGES = 50
+ANGLES_LIST = [20]
+INCLUDE_PREDCP_LIST = [False]
+NUM_IMAGES = 10
 
 for noise in NOISE_LIST:
     for angles in ANGLES_LIST:
@@ -25,10 +25,10 @@ for noise in NOISE_LIST:
             run = os.path.join(args.experiments_outputs_path, os.path.basename(run.rstrip('/')))
             log_probs = []
             for i in range(NUM_IMAGES):
-                d = torch.load(os.path.join(run, f'exact_predictive_posterior_{i}.pt'), map_location='cpu')
+                d = torch.load(os.path.join(run, f'sample_based_predictive_posterior_{i}.pt'), map_location='cpu')
                 log_probs.append(d['log_prob'])
             mean_log_prob = np.mean(log_probs)
-            print(f'kmnist exact density for noise={noise} angles={angles} include_predcp={include_predcp}')
+            print(f'kmnist sample based density for noise={noise} angles={angles} include_predcp={include_predcp}')
             print(f'mean log prob: {mean_log_prob}')
             if args.print_individual_log_probs:
                 print(f'individual log probs: {log_probs}')
